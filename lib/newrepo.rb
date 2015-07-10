14f73eaf5ae6d295dfa5238794bb49d1f34b5894
Spencer Leopold <spencer.leopold@gmail.com>
2015-07-10 18:41:13 -0400

    updated lib description

--- lib/newrepo.rb
+++ lib/newrepo.rb
@@ -3,7 +3,7 @@ require "newrepo/version"
 module Newrepo
   class Info
     def describe
-      puts 'A utility to quickly create a new bitbucket repo from the command line.'
+      puts 'A CLI to create a new repo in either github or bitbucket'
     end
   end
 end
